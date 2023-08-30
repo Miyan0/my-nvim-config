efa1bd9aa36f4f3bebd52094d7dcf2e806406f9a
Miyan0 <dr000w@gmail.com>
2023-08-30 12:58:45 -0400

    Added alpha, harpoon, treesitter, autopairs, telescope.

--- lua/miyano/plugins/bufferline.lua
+++ lua/miyano/plugins/bufferline.lua
@@ -2,6 +2,7 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
+  -- diagnostics = "nvim_lsp" | "coc",
   opts = { -- will call: require(bufferline).setup(opts)
     options = {
       mode = "tabs",
