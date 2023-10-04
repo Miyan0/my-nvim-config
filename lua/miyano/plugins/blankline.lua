73d12c33cb1cb7fd2e9ecba6d2882851a1473419
Miyan0 <dr000w@gmail.com>
2023-10-04 07:40:32 -0400

    Fixed #1, indent blankline colors are now working

--- lua/miyano/plugins/blankline.lua
+++ lua/miyano/plugins/blankline.lua
@@ -8,6 +8,13 @@ local highlight = {
   "CursorColumn",
   "Whitespace",
 }
+local hl_list = {}
+
+for i, color in pairs({ "#662121", "#767621", "#216631", "#325a5e", "#324b7b", "#562155" }) do
+  local name = "IndentBlanklineIndent" .. i
+  vim.api.nvim_set_hl(0, name, { fg = color })
+  table.insert(hl_list, name)
+end
 
 M.blankline = {
   "lukas-reineke/indent-blankline.nvim",
@@ -16,7 +23,7 @@ M.blankline = {
   config = function()
     require("ibl").setup({
       indent = {
-        highlight = highlight,
+        highlight = hl_list,
         char = "│",
         tab_char = "│",
       },
